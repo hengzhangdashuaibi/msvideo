5f6f4291dad5bbb2308c55aa60071d76c2338e10
zhangheng <zhangheng@pateo.com.cn>
2018-07-24 18:34:52 +0800

    新增微服之间的连接

---
+++ video-zuul/src/main/java/com/zhangheng/springboot/feign/UserInfoFeign.java
@@ -0,0 +1,21 @@
+package com.zhangheng.springboot.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by 蜡笔小新不爱吃青椒 on 2018/7/24.
+ *
+ * 调用ms_video的微服务的api
+ */
+@FeignClient(value = "service-video", url = "localhost:8762")
+public interface UserInfoFeign {
+
+    @RequestMapping(value = "/msvideo/user/getUserInfoByUsername",method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    String getUserInfoByUsername(@RequestParam("username") String username);
+}
