eff5bba446ca266ef939242e01c5d7bf6ec11b09
zhangheng <zhangheng@pateo.com.cn>
2018-07-25 18:08:14 +0800

    新增权限不足跳转403问题

--- video-zuul/src/main/java/com/zhangheng/springboot/custom/CustomUserDetailsService.java
+++ video-zuul/src/main/java/com/zhangheng/springboot/custom/CustomUserDetailsService.java
@@ -2,6 +2,9 @@ package com.zhangheng.springboot.custom;
 
 
 import com.zhangheng.springboot.feign.UserInfoFeign;
+import com.alibaba.fastjson.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.cas.authentication.CasAssertionAuthenticationToken;
 import org.springframework.security.core.userdetails.AuthenticationUserDetailsService;
@@ -43,20 +46,39 @@ public class CustomUserDetailsService
     @Autowired
     private UserInfoFeign userInfoFeign;
 
+    private final static Logger logger = LoggerFactory.getLogger(CustomUserDetailsService.class);
+
     @Override
     public UserDetails loadUserDetails(CasAssertionAuthenticationToken token) throws UsernameNotFoundException {
-        System.out.println("当前的用户名是："+token.getName());
+        logger.info("当前的用户名是："+token.getName());
 
         String userInfoByUsername = userInfoFeign.getUserInfoByUsername(token.getName());
+        //用户的角色权限
+        String rule=null;
+        //转json
+        JSONObject jsonObject = JSONObject.parseObject(userInfoByUsername);
 
-        System.out.println("接口返回："+userInfoByUsername);
+        if(null!=jsonObject){
+
+            if(jsonObject.getString("status").equals("200")){
+                JSONObject data = jsonObject.getJSONObject("data");
+                if(data!=null){
+                    rule=data.getString("role");
+                    logger.info("用户的角色权限:"+rule);
+                }
+
+            }
+
+        }
+
+        logger.info("接口返回："+userInfoByUsername);
 
 		/*这里我为了方便，就直接返回一个用户信息，实际当中这里修改为查询数据库或者调用服务什么的来获取用户信息*/
         UserInfo userInfo = new UserInfo();
         userInfo.setUsername(token.getName());
         userInfo.setName(token.getName());
         Set<AuthorityInfo> authorities = new HashSet<AuthorityInfo>();
-        AuthorityInfo authorityInfo = new AuthorityInfo("TEST");
+        AuthorityInfo authorityInfo = new AuthorityInfo(rule);
         authorities.add(authorityInfo);
         userInfo.setAuthorities(authorities);
         userInfo.setAccountNonLocked(true);
