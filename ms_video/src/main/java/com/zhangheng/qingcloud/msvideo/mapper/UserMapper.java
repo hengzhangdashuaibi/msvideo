092049a1a431a3546c37abf6d89842c2488cfcae
zhangheng <zhangheng@pateo.com.cn>
2018-07-26 17:50:05 +0800

    注释@select

--- ms_video/src/main/java/com/zhangheng/qingcloud/msvideo/mapper/UserMapper.java
+++ ms_video/src/main/java/com/zhangheng/qingcloud/msvideo/mapper/UserMapper.java
@@ -4,6 +4,7 @@ import com.zhangheng.qingcloud.msvideo.entity.User;
 import com.zhangheng.qingcloud.msvideo.util.YHMapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -22,5 +23,8 @@ public interface UserMapper extends YHMapper<User>{
 
    //根据用户名查询用户所拥有的角色
     List<Map<String,Object>> getUserAndRoleInfoByUsername(@Param("username") String username);
+
+    //根据用户名查询用户信息
+//     @Select("SELECT * FROM User WHERE username = #{username}")
 
 }
