432dc44acb02bbbeafa97af35f649ea2a8d4d112
zhangheng <zhangheng@pateo.com.cn>
2018-07-31 15:27:20 +0800

    开启监控功能

--- video-zuul/src/main/java/com/zhangheng/springboot/VideoZuulApplication.java
+++ video-zuul/src/main/java/com/zhangheng/springboot/VideoZuulApplication.java
@@ -8,6 +8,7 @@ import com.zhangheng.springboot.filter.routing.RoutingFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableCircuitBreaker//开启断路器功能
 @EnableZuulProxy
 @EnableFeignClients//开启使用Feign调用不同微服务的api
 public class VideoZuulApplication {
