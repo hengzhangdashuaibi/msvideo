76d46770974a937fc176e38fae2ca88e94fc1829
weipf <weipf@jzteyao.com>
2019-08-14 14:29:15 +0800

    屏蔽多余注解

--- config-server/src/main/java/com/zhangheng/config/server/ConfigServerApplication.java
+++ config-server/src/main/java/com/zhangheng/config/server/ConfigServerApplication.java
@@ -1,12 +1,12 @@
 package com.zhangheng.config.server;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by 蜡笔小新不爱吃青椒 on 2018/7/27.
@@ -16,7 +16,7 @@ import org.springframework.context.annotation.Configuration;
 @EnableConfigServer//开启config-server
 //@EnableDiscoveryClient//将其注册到服务中心
 @EnableEurekaClient
-@EnableAutoConfiguration
+//@EnableAutoConfiguration
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
